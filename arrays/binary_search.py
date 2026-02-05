d74c556561da89395b984ca75e7f725fc4498995
ronitrao222o <Ronitrao2222@gmail.com>
2026-02-05 22:00:17 +0530

    Create binary_search.py

---
+++ arrays/binary_search.py
@@ -0,0 +1,19 @@
+def binary_search(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+# Time Complexity: O(log n)
+# Space Complexity: O(1)
